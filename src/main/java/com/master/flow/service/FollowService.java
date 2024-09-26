71ba7471694ca4561d2778847206880bb3a382c1
parkseyoung1209 <rodlsdyd4@naver.com>
2024-09-26 18:34:55 +0900

    0926

--- src/main/java/com/master/flow/service/FollowService.java
+++ src/main/java/com/master/flow/service/FollowService.java
@@ -1,6 +1,8 @@
 package com.master.flow.service;
 
 import com.master.flow.model.dao.FollowDAO;
+import com.master.flow.model.dao.UserDAO;
+import com.master.flow.model.vo.Follow;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +10,10 @@ import org.springframework.stereotype.Service;
 public class FollowService {
     @Autowired
     private FollowDAO followDAO;
+
+    @Autowired UserDAO userDAO;
+
+    public void followingUser(Follow value) {
+
+    }
 }

e2e6a59351c91c77a5fff572b45524183a277e49
Dong Yeop-Lee <33149488+hjyj4841@users.noreply.github.com>
2024-09-27 08:44:26 +0900

    feat: service, controller 어노테이션 추가

--- src/main/java/com/master/flow/controller/LikesController.java
+++ src/main/java/com/master/flow/controller/LikesController.java
@@ -1,4 +1,11 @@
 package com.master.flow.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/*")
+@CrossOrigin(origins = {"*"}, maxAge=6000)
 public class LikesController {
 }

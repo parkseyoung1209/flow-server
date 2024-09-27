3ba7d0b354cc4dd1dec2b403ca5ad0267570bd2b
parkseyoung1209 <rodlsdyd4@naver.com>
2024-09-27 17:05:38 +0900

    0927

--- src/main/java/com/master/flow/controller/FollowController.java
+++ src/main/java/com/master/flow/controller/FollowController.java
@@ -1,9 +1,11 @@
 package com.master.flow.controller;
 
+import com.master.flow.model.vo.Follow;
 import com.master.flow.service.FollowService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/*")
@@ -12,4 +14,17 @@ public class FollowController {
 
     @Autowired
     private FollowService followService;
+
+    @PostMapping("/follow")
+    public ResponseEntity addFollowingRelative(Follow value) {
+        boolean logic = followService.addFollowingRelative(value);
+        // 서비스의 불리언 값에 따라서 200인지 400인지 판단
+        if(logic == true) return ResponseEntity.status(HttpStatus.OK).build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    @GetMapping("/follow")
+    public ResponseEntity viewAllFollowList() {
+        return ResponseEntity.status(HttpStatus.OK).body(followService.viewAllFollowList());
+    }
 }

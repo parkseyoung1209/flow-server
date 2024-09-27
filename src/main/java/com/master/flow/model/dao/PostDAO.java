cfdf2cd263eb012c501e6b6815a948130f517c0b
soneuron <ardor00@naver.com>
2024-09-27 17:32:43 +0900

    feat: 투표게시판 조회 및 투표 변경 기능 구현 중

--- src/main/java/com/master/flow/model/dao/PostDAO.java
+++ src/main/java/com/master/flow/model/dao/PostDAO.java
@@ -2,6 +2,12 @@ package com.master.flow.model.dao;
 
 import com.master.flow.model.vo.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PostDAO extends JpaRepository<Post, Integer> {
+    @Query("SELECT post.postType FROM Post post WHERE post.postType = :vote")
+    List<Post> findPostTypesByVote(@Param("vote") String vote);
 }
--- src/main/java/com/master/flow/service/VoteService.java
+++ src/main/java/com/master/flow/service/VoteService.java
@@ -1,7 +1,29 @@
 package com.master.flow.service;
 
+import com.master.flow.model.dao.PostDAO;
+import com.master.flow.model.dao.VoteDAO;
+import com.master.flow.model.vo.Post;
+import com.master.flow.model.vo.Vote;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class VoteService {
+
+    @Autowired
+    private VoteDAO votedao;
+    @Autowired
+    private PostDAO postdao;
+
+    // 투표 게시판 전체 조회
+    public List<Post> postVoteViewAll(Post vo) {
+        return postdao.findPostTypesByVote(vo.getPostType());
+    }
+
+    // 내가 한 투표 변경 (찬성 -> 반대 or 반대 -> 찬성)
+    public void changVote(Vote vo) {
+        votedao.save(vo);
+    }
 }

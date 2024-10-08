dabcdc99dcdf91114c5b1ec63e781398121ae798
juy0210 <juy0210@gmail.com>
2024-10-08 17:25:42 +0900

    feat : 댓글, 대댓글 로그인 회원 관련 수정
    CommentService
    CommentController

--- src/main/java/com/master/flow/controller/CommentController.java
+++ src/main/java/com/master/flow/controller/CommentController.java
@@ -2,10 +2,12 @@ package com.master.flow.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.master.flow.model.vo.Comment;
+import com.master.flow.model.vo.User;
 import com.master.flow.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -30,9 +32,14 @@ public class CommentController {
     public ResponseEntity<Comment> addComment(
             @PathVariable int parentCommentCode,
             @RequestParam("file") MultipartFile file,
-            @RequestParam("comment") String commentJson) throws IOException {
+            @RequestParam("comment") String commentJson,
+            Authentication authentication) throws IOException {
+
+        // 로그인한 사용자 정보 가져오기
+        User user = (User) authentication.getPrincipal();
 
         Comment comment = new ObjectMapper().readValue(commentJson, Comment.class);
+        comment.setUser(user);
         Comment savedComment = commentService.saveComment(parentCommentCode, comment, file);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(savedComment);

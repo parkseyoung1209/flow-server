aec8427ff191ef1b81b5ec0c2870051c2434acc1
parkseyoung1209 <rodlsdyd4@naver.com>
2024-11-06 17:22:30 +0900

    1106
    주석 첨부 및 유틸리티 패키지 만듦

--- src/main/java/com/master/flow/follow/util/KoreanStringUtil.java
+++ src/main/java/com/master/flow/follow/util/KoreanStringUtil.java
@@ -7,15 +7,17 @@ import java.util.Map;
 @Component
 public class KoreanStringUtil {
 
+    // 초성 모음
     public String[] INITIALS = {
             "ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ", "ㄹ", "ㅁ", "ㅂ", "ㅃ", "ㅅ", "ㅆ",
             "ㅇ", "ㅈ", "ㅉ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"
     };
+    // 종성 모음
     public String[] FINAL_CONSONANTS = {
             "",   "ㄱ", "ㄲ", "ㄳ", "ㄴ", "ㄵ", "ㄶ", "ㄷ", "ㄹ", "ㄺ", "ㄻ", "ㄼ", "ㄽ",
             "ㄾ", "ㄿ", "ㅀ", "ㅁ", "ㅂ", "ㅄ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"
     };
-
+    // 복합 조성을 키로 갖고 그 조성을 나눈 문자열을 밸류로 갖는 맵
     public Map<String, String> COMPLEX_CONSONANTS = Map.ofEntries(
             Map.entry(FINAL_CONSONANTS[3], "ㄱㅅ"),
             Map.entry(FINAL_CONSONANTS[5], "ㄴㅈ"),
@@ -29,28 +31,32 @@ public class KoreanStringUtil {
             Map.entry(FINAL_CONSONANTS[15], "ㄹㅎ"),
             Map.entry(FINAL_CONSONANTS[18], "ㅂㅅ")
     );
-
+    //사용자가 입력한 문자열이 어떠한 문자열인지 판독 후 정수형 데이터 발사
     public int isKoreanConsonant(String key) {
         if (key == null || key.trim().isEmpty()) {
             return 0;
-        }
+        } // 공백 or 빈문자열은 0
         for(int i=0; i<key.length(); i++) {
             int unicode = key.charAt(key.length()-1) - 0xAC00;
             int finalIndex = unicode % 28;
             if (key.length() > 1) {
-                if(key.charAt(i)< 0x3131) return 0;
-
+                if(key.charAt(i)< 0x3131) return 0; // 한글이 아닐 시에 바로 0
+                // 길이가 2 이상인 문자열의 마지막 글자가 초성이라면
                 if(key.charAt(key.length()-1) >= 0x3131 && key.charAt(key.length()-1) <= 0x314E) return 3;
+                // 각 문자열들의 종성이 존재하지 않는다면
                 else if(finalIndex == 0) return 3;
+                // 죄다 한글에 종성까지 있다면
                 else return 5;
             }
         }
+        //단일 문자에 초성이다
         if(key.length() == 1 && (key.charAt(0) >= 0x3131 && key.charAt(0) <= 0x314E)) {
             return 1;
         }
+        //단일 문자일 때 초성+중성이냐 초성+중성+종성이냐
         if (key.length() == 1 && key.charAt(0) >= 0xAC00 && key.charAt(0) <= 0xD7A3) {
-            int unicode = key.charAt(0) - 0xAC00;
-            int finalIndex = unicode % 28;
+            int unicode = key.charAt(0) - 0xAC00; // 해당 문자의 유니코드 추출
+            int finalIndex = unicode % 28; // 종성 인덱스 구하기(종성이 존재한다면 0이 나올 수가 없음)
             if (finalIndex == 0) {
                 return 2;
             } else return 4;
@@ -60,35 +66,42 @@ public class KoreanStringUtil {
 
     public String sliceKorean(String key) {
         if(!key.matches(".*\\s.*")) {
-            if (isKoreanConsonant(key) == 4) {
+            // 현재 입력값이 종성이 있는 단일 문자인 경우일 때
+            if (isKoreanConsonant(key) == 4) { // isKoreanConsonant의 값으로 판독
                 int unicode = key.charAt(0) - 0xAC00;
                 int initialIndex = unicode / (21 * 28); // 초성 인덱스 추출
                 int medialIndex = (unicode % (21 * 28)) / 28; // 중성 인덱스 추출
-                int finalIndex = unicode % 28;
+                int finalIndex = unicode % 28; // 종성 인덱스 추출
 
                 StringBuilder sliceTextBuilder = new StringBuilder();
+                // 현재 문자에서 초성+중성만 있는 문자를 새로 만듦
                 String firstChar = String.valueOf((char) (0xAC00 + (initialIndex * 21 * 28) + (medialIndex * 28)));
+                // 종성 배열에서 위에 구한 인덱스 값으로 종성만 추출
                 String secondChar = FINAL_CONSONANTS[finalIndex];
+                // 복합종성일 시
                 if (COMPLEX_CONSONANTS.containsKey(secondChar)) {
-                    secondChar = COMPLEX_CONSONANTS.get(secondChar);
-                    // secondChar 을 잘라서 배열의 무언가와 비교를 하고 그 배열에 맞는 인덱스를 저기 더해줘야함
-                    String secondCharBefore = secondChar.substring(0, 1);
+                    secondChar = COMPLEX_CONSONANTS.get(secondChar); // 종성을 두 문자열로 나눈다
+                    String secondCharBefore = secondChar.substring(0, 1); // 그 중 첫번째 문자열을 추출
                     int secondCharIndex = 0;
                     for (int i = 0; i < FINAL_CONSONANTS.length; i++) {
                         if (secondCharBefore.equals(FINAL_CONSONANTS[i])) {
                             secondCharIndex = i;
                             break;
                         }
-                    }
-                    firstChar = String.valueOf((char) (firstChar.charAt(0) + secondCharIndex));
-                    secondChar = secondChar.substring(1, 2);
+                    } // 사용자가 입력한 단일 문자의 종성과 substring한 값과 일치할 때의 i값을 추출하여 secondCharIndex에 넣는다
+                    firstChar = String.valueOf((char) (firstChar.charAt(0) + secondCharIndex)); // 첫번째 글자는 그대로
+                    secondChar = secondChar.substring(1, 2); // 복합종성을 나눈 값의 두번째 문자를 다시 합친다
+                    // 만약 입력값이 '좂'이라면 "좁ㅅ"이라는 문자열로 바뀌는 것
                 }
+                // if 조건에 해당되지 않는 단일 종성이라면 그대로 도달한다
+                // 예를 들어 입력값이 '종' 이라면 '조ㅇ'이 된다.
                 sliceTextBuilder.append(firstChar);
                 sliceTextBuilder.append(secondChar);
 
                 String sliceText = sliceTextBuilder.toString();
                 return sliceText;
             } else if (isKoreanConsonant(key) == 5) {
+                // 단일문자가 아니고 마지막 문자가 초성도 아닐 경우...
                 StringBuilder sliceTextBuilder = new StringBuilder();
                 for (char ch : key.toCharArray()) {
                     if(!(ch < 0xAC00)) {
@@ -98,19 +111,20 @@ public class KoreanStringUtil {
                         int finalIndex = unicode % 28;
 
                         if (finalIndex == 0) {
-                            sliceTextBuilder.append(ch);
+                            sliceTextBuilder.append(ch); // 문자가 초성+중성 조합일 경우 그대로
                         } else {
                             String firstChar = String.valueOf((char) (0xAC00 + (initialIndex * 21 * 28) + (medialIndex * 28)));
                             String secondChar = FINAL_CONSONANTS[finalIndex];
                             sliceTextBuilder.append(firstChar);
                             sliceTextBuilder.append(secondChar);
-                        }
+                        } // 아니라면 초성+중성과 종성을 별도로 합친 문자열이 탄생한다
+                        //예를 들어 사용자가 입력한 값이 '테슽'이라고 한다면 '테스ㅌ'이 된다
                     }
                 }
                 String sliceText = sliceTextBuilder.toString();
                 return sliceText;
             }
         }
-        return key;
+        return key; // 어떤 경우에도 도달하지 않는다면 기존 입력값 그대로 리턴한다
     }
 }
--- src/main/java/com/master/flow/service/FollowService.java
+++ src/main/java/com/master/flow/service/FollowService.java
@@ -82,10 +82,9 @@ public class FollowService {
             return false; // 아니면 false하고 컨트롤러 ㄱㄱ
         }
     }
-
+    // 처음 검색어가 존재하지 않거나 공백문자열일 때 밸류없는 불리언빌더 생성을 위함
     private BooleanBuilder followBuilder(String key) {
         QUser qUser = QUser.user;  // QueryDSL로 생성된 QUser 객체
-
         BooleanBuilder booleanBuilder = new BooleanBuilder();
 
         if (key != null && !key.trim().isEmpty()) {
@@ -96,7 +95,7 @@ public class FollowService {
         }
         return booleanBuilder;
     }
-
+    // 이후 스트링 문자열이 온전히 포함된 리스트는 그대로 가져오고, 검색어가 포함되지 않으면 where 조건에만 맞는 리스트 가져오기
     private List<User> followingUserList(BooleanBuilder booleanBuilder, int code) {
         QUser qUser = QUser.user;  // QueryDSL로 생성된 QUser 객체
         QFollow qFollow = QFollow.follow;
@@ -119,7 +118,7 @@ public class FollowService {
         }
         return users;
     }
-
+    // 위와 같음
     private List<User> followerUserList(BooleanBuilder booleanBuilder, int code) {
         QUser qUser = QUser.user;  // QueryDSL로 생성된 QUser 객체
         QFollow qFollow = QFollow.follow;
@@ -142,6 +141,7 @@ public class FollowService {
         }
         return users;
     }
+    //위에 만들어진 불리언빌더와 팔로잉/팔로우리스트를 조합한 유저 객체 리스트
     private List<User> filteredUsers(String key, int code) {
         BooleanBuilder followFilter = followBuilder(key);
         return followingUserList(followFilter, code);
@@ -150,27 +150,25 @@ public class FollowService {
         BooleanBuilder followFilter = followBuilder(key);
         return followerUserList(followFilter,code);
     }
-    private List<UserDTO> initialUserDTOList(List<User> initialSearchUser, int code) {
-        return initialSearchUser.stream()
-                .map(user -> {
-                    boolean logic = checkLogic(code, user.getUserCode());
-                    return new UserDTO(user, logic);
-                })
-                .toList();
-    }
+
+    //헬퍼 클래스의 convertToInitialsFromName를 기반으로 검색어에 매칭되는 닉네임들을 가진 filtereduser의 유저들만 추출후 initialSearchUser 리스트에 넣는 과정을 개별 메서드들로 분리
+    //containsKeyword ~ switchOfFollow까지 한 과정...
     private void containsKeyword(int i, List<String> nickNameList, List<User> filteredUsers, List<User> initialSearchUser) {
             String matchingName = nickNameList.get(i);
             filteredUsers.stream()
                     .filter(user -> user.getUserNickname().contains(matchingName)) // 닉네임 일치 여부 확인
                     .findFirst()
                     .ifPresent(initialSearchUser::add);
+            // 필터드 유저는 검색 입력값이 별도의 처리 없이 포함 가능하게끔 입력했다면 해당하는 유저만 가져오고, 아닌 경우엔 기본 WHERE 조건만 있는 팔로워/팔로위 리스트를 가져온다.
+            // 여기서 밑에 헬퍼클래스로 재구성한 닉네임 리스트와 검색어가 일치하는 순간에만 위 스트림 문을 실행한다.
+            // 헬퍼클래스의 인덱스와 닉네임리스트의 인덱스가 일치하기 때문에 가능하다
     }
 
     private void initialSearchUser(List<User> filteredUsers, int num, String keyword, String keywordOrNull, List<String> nickNameList, List<User> initialSearchUser) {
         List<String> userNickNameList = ciList.convertToInitialsFromName(filteredUsers, num, keywordOrNull);
         for (int i = 0; i < userNickNameList.size(); i++) {
             if (userNickNameList.get(i).contains(keyword)) {
-                containsKeyword(i, nickNameList, filteredUsers, initialSearchUser);
+                containsKeyword(i, nickNameList, filteredUsers, initialSearchUser); // 위에 메서드 넣기
             }
         }
     }
@@ -180,6 +178,7 @@ public class FollowService {
             if (filteredUsers.get(i).getUserNickname().contains(keyword)) {
                 containsKeyword(i, nickNameList, filteredUsers, initialSearchUser);
             } else if (userNickNameList.get(i).contains(filteringChar) || userNickNameList.get(i).contains(keyword)) {
+                // 종성이 있는 경우를 filteringChar와 ciList.convertToInitialsFromName로 처리한다.
                 containsKeyword(i, nickNameList, filteredUsers, initialSearchUser);
             }
         }
@@ -189,6 +188,7 @@ public class FollowService {
         for (int i = 0; i < filteredUsers.size(); i++) {
             if (filteredUsers.get(i).getUserNickname().contains(keyword)) {
                 containsKeyword(i, nickNameList, filteredUsers, initialSearchUser);
+                // else if의 두번째 조건은 2글자 이상의 한글 문자열에서 첫글자가 같고, 끝 글자를 koreanStringUtil의 slicekorean으로 자른 문자가 포함되는 경우이다.
             } else if (userNickNameList.get(i).contains(keyword) || (
                     userNickNameList.get(i).contains(String.valueOf(keyword.charAt(0)))
                             && userNickNameList.get(i).contains(String.valueOf(krUtil.sliceKorean(String.valueOf(keyword.charAt(keyword.length()-1))).charAt(0))))) {
@@ -196,64 +196,74 @@ public class FollowService {
             }
         }
     }
+    private List<UserDTO> initialUserDTOList(List<User> initialSearchUser, int code) {
+        return initialSearchUser.stream()
+                .map(user -> {
+                    boolean logic = checkLogic(code, user.getUserCode());
+                    return new UserDTO(user, logic);
+                })
+                .toList();
+    }
     private FollowDTO switchOfFollowDto(List<User> filteredUsers, String keyword, List<String> nickNameList, List<User> initialSearchUser, int code) {
         switch (krUtil.isKoreanConsonant(keyword)) {
             case 1: {
-                // keyornull = null, keyword는 keyword num=1
+                // 문자/문자열이 초성으로만 조합되었을 때 유저의 닉네임을 초성으로만 재구축
                 initialSearchUser(filteredUsers, 1, keyword, null, nickNameList, initialSearchUser);
                 List<UserDTO> initialUserDTOList = initialUserDTOList(initialSearchUser, code);
                 return new FollowDTO(initialUserDTOList.size(), initialUserDTOList);
             }
             case 2: {
-                // keyornull = null num=2 keyword=keyword
+                // 문자/문자열이 유저들의 닉네임의 초성+중성과 일치하는 경우 재구축
                 initialSearchUser(filteredUsers, 2,keyword,null,nickNameList,initialSearchUser);
                 List<UserDTO> initialUserDTOList = initialUserDTOList(initialSearchUser, code);
                 return new FollowDTO(initialUserDTOList.size(), initialUserDTOList);
             }
             case 3: {
-                // keyornull = keyword num=2 keyword=keyword
+                // 문자열의 다음 문자가 초성일 경우 리스트 재구성
+                // 예를 들어 검색어가 '신ㄱ' 이고 풀네임은 '신고테스트' 일 경우 '신ㄱㅌㅅㅌ'로 변환
                 initialSearchUser(filteredUsers, 3, keyword, keyword, nickNameList,initialSearchUser);
                 List<UserDTO> initialUserDTOList = initialUserDTOList(initialSearchUser, code);
                 return new FollowDTO(initialUserDTOList.size(), initialUserDTOList);
             }
             case 4: {
+                // 단일 문자가 종성이 있을 경우 slicekorean 메서드로 종성 해체 후 리스트와 비교
                 finalConsonantSearchCompletedUser(filteredUsers, keyword, nickNameList, initialSearchUser, String.valueOf(krUtil.sliceKorean(keyword).charAt(0)));
                 List<UserDTO> initialUserDTOList = initialUserDTOList(initialSearchUser, code);
                 return new FollowDTO(initialUserDTOList.size(), initialUserDTOList);
             }
             case 5 : {
+                // 문자열의 끝자리가 초성+중성 이상일 경우 처리...
                 doubleDigitSearchCompletedUser(filteredUsers, keyword, nickNameList, initialSearchUser);
                 List<UserDTO> initialUserDTOList = initialUserDTOList(initialSearchUser, code);
                 return new FollowDTO(initialUserDTOList.size(), initialUserDTOList);
             }
             default: {
+                // 검색어가 없다면 전체 리스트 그대로
                 List<UserDTO> userDTOList = initialUserDTOList(filteredUsers, code);
                 return new FollowDTO(userDTOList.size(), userDTOList);
             }
         }
     }
-    //내가 팔로우한 인간들의 수와 인간들 전체 목록 dto발사
-    public FollowDTO viewMyFollower(int followingUserCode, String key) {
-        if(key != null) {
-            key=key.trim();
+    // 최종적으로 걸러진 유저리스트와 팔로우 여부를 followDTO로 묶어서 보내기
+    public FollowDTO viewMyFollower(int followingUserCode, String keyword) {
+        if(keyword != null) {
+            keyword=keyword.trim();
         }
-        List<User> filteredUsers = filteredUsers(key, followingUserCode);
+        List<User> filteredUsers = filteredUsers(keyword, followingUserCode);
         List<String> nickNameList = ciList.nickNameList(filteredUsers);
         List<User> initialSearchUser = new ArrayList<>();
 
-        String keyword = key;
         return switchOfFollowDto(filteredUsers, keyword, nickNameList, initialSearchUser, followingUserCode);
     }
-    //위랑 반대
-    public FollowDTO followMeUsers (int followerUserCode, String key) {
-        if(key != null) {
-            key=key.trim();
+    // 위랑 같음
+    public FollowDTO followMeUsers (int followerUserCode, String keyword) {
+        if(keyword != null) {
+            keyword = keyword.trim();
         }
-        List<User> filteredUsers = filteredUsers2(key, followerUserCode);
+        List<User> filteredUsers = filteredUsers2(keyword, followerUserCode);
         List<String> nickNameList = ciList.nickNameList(filteredUsers);
         List<User> initialSearchUser = new ArrayList<>();
 
-        String keyword = key;
         return switchOfFollowDto(filteredUsers, keyword, nickNameList, initialSearchUser, followerUserCode);
     }
 
